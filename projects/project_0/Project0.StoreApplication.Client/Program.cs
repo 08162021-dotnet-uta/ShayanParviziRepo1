ee3ee08e9c73ceccdb90308ab77a43ff2b522e71
Shayan Parvizi <shayan.parvizi@revature.com>
2021-09-01 01:05:54 -0500

    Atleast its running now

--- projects/project_0/Project0.StoreApplication.Client/Program.cs
+++ projects/project_0/Project0.StoreApplication.Client/Program.cs
@@ -17,6 +17,7 @@ namespace Project0.StoreApplication.Client
   {
     private static readonly CustomerSingleton _customerSingleton = CustomerSingleton.Instance;
     private static readonly StoreSingleton _storeSingleton = StoreSingleton.Instance;
+    private static readonly ProductSingleton _productSingleton = ProductSingleton.Instance;
     private const string _logFilePath = @"/home/fred/revature/fred_repo/data/logs.txt";
 
     /// <summary>
@@ -26,13 +27,14 @@ namespace Project0.StoreApplication.Client
     private static void Main(string[] args)
     {
       Log.Logger = new LoggerConfiguration().WriteTo.File(_logFilePath).CreateLogger();
-      DataAdapter data = new DataAdapter();
-            //HelloSQL();
-            var customer = data.Customers.ToList();
-            foreach(var c in customer)
-            {
-                Console.WriteLine($"{c.Name}, {c.CustomerId}");
-            }
+      //   //DataAdapter data = new DataAdapter();
+      //         //HelloSQL();
+      //  //       var customer = data.Customers.ToList();
+      //         foreach(var c in customer)
+      //         {
+      //             Console.WriteLine($"{c.Name}, {c.CustomerId}");
+      //         }
+      Run();
     }
 
     /// <summary>
@@ -49,16 +51,16 @@ namespace Project0.StoreApplication.Client
 
       var customer = _customerSingleton.Customers[Capture<Customer>(_customerSingleton.Customers)];
       var store = _storeSingleton.Stores[Capture<Store>(_storeSingleton.Stores)];
-
+      var product = _productSingleton.Products[Capture<Product>(_productSingleton.Products)];
       Console.WriteLine(customer);
     }
 
     /// <summary>
     /// 
     /// </summary>
-    private static void Output<T>(List<T> data) where T : class
+    private static void Output<Store>(List<Store> data)
     {
-      Log.Information($"method: Output<{typeof(T)}>()");
+      Log.Information($"method: Output<{typeof(Store)}>()");
 
       var index = 0;
 
@@ -80,20 +82,39 @@ namespace Project0.StoreApplication.Client
       Console.Write("make selection: ");
 
       int selected = int.Parse(Console.ReadLine());
+      Computer c = new Computer();
+      Monitor m = new Monitor();
+      Pickles p = new Pickles();
+      if (selected == 1)
+      {
+        Console.WriteLine("Select a store:");
+
+
+      }
+      else if (selected == 2)
+      {
+        Console.WriteLine(p);
+      }
+      else if (selected == 3)
+      {
+        Console.WriteLine(c);
+        Console.WriteLine(m);
+      }
 
       return selected - 1;
     }
 
-    private static void HelloSQL()
-    {
-      var def = new DemoEF();
 
-      def.SetCustomer(new Customer());
+    // private static void HelloSQL()
+    // {
+    //   var def = new DemoEF();
 
-      foreach (var item in def.GetCustomers())
-      {
-        Console.WriteLine(item);
-      }
-    }
+    //   def.SetCustomer(new Customer());
+
+    //   foreach (var item in def.GetCustomers())
+    //   {
+    //     Console.WriteLine(item);
+    //   }
+    // }
   }
 }
